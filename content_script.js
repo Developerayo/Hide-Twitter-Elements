7e235fc24a740cf6fbbbda5ec6686597d8976c22
Shodipo Ayomide <shodipovi@gmail.com>
2022-12-24 23:05:19 +0100

    chore: clean up and fix analytics issue

--- content_script.js
+++ content_script.js
@@ -1,10 +1,7 @@
 $(function () {
   setInterval(() => {
     chrome.storage.local.get(["running_status_analytics"], function (result) {
-      if (
-        typeof result.running_status_analytics != "undefined" &&
-        result.running_status_analytics != ""
-      ) {
+      if (typeof result.running_status_analytics != "undefined" && result.running_status_analytics != "") {
         if (result.running_status_analytics == "start") {
           hideAnalyticsOptions();
         }
@@ -12,10 +9,7 @@ $(function () {
     });
 
     chrome.storage.local.get(["running_status_comments"], function (result) {
-      if (
-        typeof result.running_status_comments != "undefined" &&
-        result.running_status_comments != ""
-      ) {
+      if (typeof result.running_status_comments != "undefined" && result.running_status_comments != "") {
         if (result.running_status_comments == "start") {
           hideCommentsOptions();
         }
@@ -23,10 +17,7 @@ $(function () {
     });
 
     chrome.storage.local.get(["running_status_retweets"], function (result) {
-      if (
-        typeof result.running_status_retweets != "undefined" &&
-        result.running_status_retweets != ""
-      ) {
+      if (typeof result.running_status_retweets != "undefined" && result.running_status_retweets != "") {
         if (result.running_status_retweets == "start") {
           hideRetweetsOptions();
         }
@@ -34,60 +25,40 @@ $(function () {
     });
 
     chrome.storage.local.get(["running_status_likes"], function (result) {
-      if (
-        typeof result.running_status_likes != "undefined" &&
-        result.running_status_likes != ""
-      ) {
+      if (typeof result.running_status_likes != "undefined" && result.running_status_likes != "") {
         if (result.running_status_likes == "start") {
           hideLikesOptions();
         }
       }
     });
-  }, 700);
+  }, 100);
 });
 
+
+
+
 function hideAnalyticsOptions() {
-  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find(
-    'article[data-testid="tweet"]'
-  );
+  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find('article[data-testid="tweet"]');
   $first_tweet_li.each(function (index, item) {
-    $(this).find('a[aria-label="View Tweet analytics"][role="link"]').hide();
-    // $(this).find('div[aria-label][data-testid="retweet"]').hide();
-    // $(this).find('div[aria-label][data-testid="reply"]').hide();
-    // $(this).find('div[aria-label][data-testid="like"]').hide();
+    $(this).find('a[aria-label][role="link"]').hide();
   });
 }
 function hideCommentsOptions() {
-  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find(
-    'article[data-testid="tweet"]'
-  );
+  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find('article[data-testid="tweet"]');
   $first_tweet_li.each(function (index, item) {
-    // $(this).find('a[aria-label="View Tweet analytics"][role="link"]').hide();
-    // $(this).find('div[aria-label][data-testid="retweet"]').hide();
     $(this).find('div[aria-label][data-testid="reply"]').hide();
-    //$(this).find('div[aria-label][data-testid="like"]').hide();
   });
 }
 function hideRetweetsOptions() {
-  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find(
-    'article[data-testid="tweet"]'
-  );
+  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find('article[data-testid="tweet"]');
   $first_tweet_li.each(function (index, item) {
-    // $(this).find('a[aria-label="View Tweet analytics"][role="link"]').hide();
     $(this).find('div[aria-label][data-testid="retweet"]').hide();
-    // $(this).find('div[aria-label][data-testid="reply"]').hide();
-    // $(this).find('div[aria-label][data-testid="like"]').hide();
   });
 }
 
 function hideLikesOptions() {
-  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find(
-    'article[data-testid="tweet"]'
-  );
+  $first_tweet_li = $('div[data-testid="cellInnerDiv"]').find('article[data-testid="tweet"]');
   $first_tweet_li.each(function (index, item) {
-    // $(this).find('a[aria-label="View Tweet analytics"][role="link"]').hide();
-    // $(this).find('div[aria-label][data-testid="retweet"]').hide();
-    // $(this).find('div[aria-label][data-testid="reply"]').hide();
     $(this).find('div[aria-label][data-testid="like"]').hide();
   });
 }
--- popop.js
+++ popop.js
@@ -5,122 +5,112 @@ let retweets_tag_btn = $("#on-off-retweets");
 //let frnd_list_btn = $("#frnd_list");
 
 analytics_tag_btn.click(function () {
-  if ($(this).prop("checked") == true) {
+  if ($(this).prop('checked') == true) {
     status_analytics = "start";
-    console.log("start");
-    chrome.storage.local.set({ running_status_analytics: status_analytics });
+    console.log('start');
+    chrome.storage.local.set({ 'running_status_analytics': status_analytics });
     reloadTwitterTabs();
   } else {
-    status_analytics = "";
-    chrome.storage.local.set({ running_status_analytics: status_analytics });
+    status_analytics = ""
+    chrome.storage.local.set({ 'running_status_analytics': status_analytics });
     reloadTwitterTabs();
   }
 });
 
 comments_tag_btn.click(function () {
-  if ($(this).prop("checked") == true) {
+  if ($(this).prop('checked') == true) {
     status_comment = "start";
-    console.log("start");
-    chrome.storage.local.set({ running_status_comments: status_comment });
+    console.log('start');
+    chrome.storage.local.set({ 'running_status_comments': status_comment });
     reloadTwitterTabs();
   } else {
-    status_comment = "";
-    chrome.storage.local.set({ running_status_comments: status_comment });
+    status_comment = ""
+    chrome.storage.local.set({ 'running_status_comments': status_comment });
     reloadTwitterTabs();
   }
 });
 
 likes_tag_btn.click(function () {
-  if ($(this).prop("checked") == true) {
+  if ($(this).prop('checked') == true) {
     status_likes = "start";
-    console.log("start");
-    chrome.storage.local.set({ running_status_likes: status_likes });
+    console.log('start');
+    chrome.storage.local.set({ 'running_status_likes': status_likes });
     reloadTwitterTabs();
   } else {
-    status_likes = "";
-    chrome.storage.local.set({ running_status_likes: status_likes });
+    status_likes = ""
+    chrome.storage.local.set({ 'running_status_likes': status_likes });
     reloadTwitterTabs();
   }
 });
 
 retweets_tag_btn.click(function () {
-  if ($(this).prop("checked") == true) {
+  if ($(this).prop('checked') == true) {
     status_retweets = "start";
-    console.log("start");
-    chrome.storage.local.set({ running_status_retweets: status_retweets });
+    console.log('start');
+    chrome.storage.local.set({ 'running_status_retweets': status_retweets });
     reloadTwitterTabs();
   } else {
-    status_retweets = "";
-    chrome.storage.local.set({ running_status_retweets: status_retweets });
+    status_retweets = ""
+    chrome.storage.local.set({ 'running_status_retweets': status_retweets });
     reloadTwitterTabs();
   }
 });
 
 chrome.storage.local.get(["running_status_analytics"], function (result) {
   console.log(result);
-  if (
-    typeof result.running_status_analytics != "undefined" &&
-    result.running_status_analytics != ""
-  ) {
+  if (typeof result.running_status_analytics != "undefined" && result.running_status_analytics != "") {
     if (result.running_status_analytics == "start") {
-      $("#on-off-analytics").attr("checked", "checked");
+      $('#on-off-analytics').attr("checked", "checked");
     } else {
-      $("#on-off-analytics").removeAttr("checked");
+      $('#on-off-analytics').removeAttr("checked");
     }
   } else {
-    $("#on-off-analytics").removeAttr("checked");
-    console.log("unchecked");
+    $('#on-off-analytics').removeAttr("checked");
+    console.log('unchecked');
   }
 });
 
 chrome.storage.local.get(["running_status_comments"], function (result) {
   console.log(result);
-  if (
-    typeof result.running_status_comments != "undefined" &&
-    result.running_status_comments != ""
-  ) {
+  if (typeof result.running_status_comments != "undefined" && result.running_status_comments != "") {
     if (result.running_status_comments == "start") {
-      $("#on-off-comments").attr("checked", "checked");
+      $('#on-off-comments').attr("checked", "checked");
     } else {
-      $("#on-off-comments").removeAttr("checked");
+      $('#on-off-comments').removeAttr("checked");
     }
   } else {
-    $("#on-off-comments").removeAttr("checked");
-    console.log("unchecked");
+    $('#on-off-comments').removeAttr("checked");
+    console.log('unchecked');
   }
 });
 
+
+
 chrome.storage.local.get(["running_status_likes"], function (result) {
   console.log(result);
-  if (
-    typeof result.running_status_likes != "undefined" &&
-    result.running_status_likes != ""
-  ) {
+  if (typeof result.running_status_likes != "undefined" && result.running_status_likes != "") {
     if (result.running_status_likes == "start") {
-      $("#on-off-like").attr("checked", "checked");
+      $('#on-off-like').attr("checked", "checked");
     } else {
-      $("#on-off-like").removeAttr("checked");
+      $('#on-off-like').removeAttr("checked");
     }
   } else {
-    $("#on-off-like").removeAttr("checked");
-    console.log("unchecked");
+    $('#on-off-like').removeAttr("checked");
+    console.log('unchecked');
   }
 });
 
 chrome.storage.local.get(["running_status_retweets"], function (result) {
   console.log(result);
-  if (
-    typeof result.running_status_retweets != "undefined" &&
-    result.running_status_retweets != ""
-  ) {
+  if (typeof result.running_status_retweets != "undefined" && result.running_status_retweets != "") {
     if (result.running_status_retweets == "start") {
-      $("#on-off-retweets").attr("checked", "checked");
+      $('#on-off-retweets').attr("checked", "checked");
     } else {
-      $("#on-off-retweets").removeAttr("checked");
+      $('#on-off-retweets').removeAttr("checked");
     }
   } else {
-    $("#on-off-retweets").removeAttr("checked");
-    console.log("unchecked");
+    $('#on-off-retweets').removeAttr("checked");
+    console.log('unchecked');
   }
 });
 
@@ -130,7 +120,7 @@ function reloadTwitterTabs() {
     windows.forEach(function (window) {
       if (window.type == "normal") {
         window.tabs.forEach(function (tab) {
-          if (tab.url && tab.url.indexOf("twitter.com") != -1) {
+          if (tab.url && (tab.url.indexOf('twitter.com') != -1)) {
             chrome.tabs.reload(tab.id);
           }
         });
